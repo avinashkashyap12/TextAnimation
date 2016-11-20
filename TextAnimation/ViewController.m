3340a6dff4a498efe4d9cfc606782c224cdfc1e6
Avinash Kashyap <avinash@headerlabs.com>
2016-11-21 00:17:32 +0530

    Add comments.

--- TextAnimation/ViewController.m
+++ TextAnimation/ViewController.m
@@ -40,34 +40,52 @@
 -(IBAction)buttonAction:(UIButton *)sender{
     switch (sender.tag) {
         case 1:
+            //display text with scale transform
+            
             [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithCenterAnimation withSpinAnimation:YES];
             break;
         case 2:
+            
+            // display text animation view from top of the screen to declared frame with simple animation.
             [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithTopAnimation withSpinAnimation:NO];
             break;
+            
         case 3:
+            // display text animation view from left of the screen to declared frame with spin animation.
+            
             [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithLeftAnimation withSpinAnimation:YES];
             break;
         case 4:
-            [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithAlpha withSpinAnimation:YES];
+            // display text animation view from alpha value animation with spin animation.
             
+            [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithAlpha withSpinAnimation:YES];
             break;
         case 5:
+            // display text animation view from the bottom of the screen to declared frame with spin animation.
+            
             [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithBottomAnimation withSpinAnimation:YES];
             break;
         case 6:
+            // display text animation view from right of the screen to declared frame with out spin animation.
+            
             [textAnimationView displayMessageViewWithPresentationType:TextAnimationViewPresentWithRightAnimation withSpinAnimation:NO];
             break;
         case 7:
+            // display text animation view with marquee animation from right
+            
             [textAnimationView setTextMarquee:YES withDirectionType:TextMarqueeFromRight];
             break;
         case 8:
+            // display text animation view with marquee animation from left
             [textAnimationView setTextMarquee:YES withDirectionType:TextMarqueeFromLeft];
             break;
         case 9:
+            // display text animation view with marquee animation from Top
             [textAnimationView setTextMarquee:YES withDirectionType:TextMarqueeFromTop];
             break;
         case 10:
+            // display text animation view with character animation(one by one character animation)
+            
             [textAnimationView displayTextWithTypingAnimation:@"It's a sample marquee message."];
             break;
             
